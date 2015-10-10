5531f0ac66a36640401fb6fc21ea56d68ba41a59
Thomas Leskin <tleskin@gmail.com>
2015-10-10 15:12:17 -0600

    Initial commit

---
+++ gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: {
+          'public/stylesheets/style.css' : 'sass/style.scss'
+        }
+      }
+    },
+
+    watch: {
+      source: {
+        files: ['sass/**/*.scss'],
+        tasks: ['sass'],
+        options: {
+          livereload: true, // needed to run LiveReload
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.registerTask('default', ['sass']);
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+};
